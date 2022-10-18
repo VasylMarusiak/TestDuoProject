51a1cad8c8f0c286e3c4f0881ca5fb8caca165d5
Vasyl Marusiak <vmarusiak@sunpowercorp.com>
2022-10-18 16:04:06 +0300

    Disabled run tests for class

--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -58,13 +58,13 @@ public class TestClass extends SelenoidRunner {
     }
 
    // @Test
-    private void verifyTestLoggerMessages() {
-        Logger logger = LogManager.getLogger();
-        logger.trace("This is a trace message");
-        logger.debug("This is a debug message");
-        logger.info("This is an info message");
-        logger.warn("This is a warn message");
-        logger.error("This is an error message");
-        logger.fatal("This is a fatal message");
-    }
+//    private void verifyTestLoggerMessages() {
+//        Logger logger = LogManager.getLogger();
+//        logger.trace("This is a trace message");
+//        logger.debug("This is a debug message");
+//        logger.info("This is an info message");
+//        logger.warn("This is a warn message");
+//        logger.error("This is an error message");
+//        logger.fatal("This is a fatal message");
+//    }
 }
